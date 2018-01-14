53d8da9819166c54a4ed6ee006b5330ce0f5c2cf
Francesco Wofford <francescowofford@gmail.com>
2018-01-14 15:21:22 +0100

    first commit

---
+++ WpfAppLogin/ViewModel/BaseViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using PropertyChanged;
+using System.ComponentModel;
+
+namespace WpfAppLogin.ViewModel
+{
+    public class BaseViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged = (sender, e) => { };
+
+        public void OnPropertyChanged(String name)
+        {
+            PropertyChanged(this, new PropertyChangedEventArgs(name));
+        }
+
+    }
+}
