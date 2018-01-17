6ee29d0b73f257345daad73b09213eac964ccb39
Pantegana <elsirokk@gmail.com>
2018-01-17 09:41:23 +0100

    lib files

---
+++ AlarmLib/Map.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+
+namespace MapLibrary
+{
+    /// <summary>
+    /// classe che controlla la mappa nella sala controllo, contiene una lista di centraline
+    /// </summary>
+    public class Map
+    {
+        public Image map { get; set; }
+        public List<ClientInterface> allarmClients { get; set; }
+        
+        public void render()
+        {
+            allarmClients.ForEach(c => drawClient(c, c.getAlarmState()));
+        }
+
+        public void drawClient(ClientInterface c, Color col) { }
+    }
+}
---
+++ WpfAppLogin/AlarmLib/AlarmController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Device.Location;
+
+namespace MapLibrary
+{
+    /// <summary>
+    ///  classe che rappresenta la centralina controllo allarmi, connessa ai sensori
+    /// </summary>
+    public class AlarmController
+    {
+        public GeoCoordinate mapCoordinates {get; set;}
+        public List<SensorInterface> sensors { get; set; }
+
+        // gestire caso lista nulla
+        public List<SensorInterface> readSensors()
+        {
+            return sensors.Where(s => s.getAlarm()).ToList();
+        }
+
+        public void allarm(string sensorID, string eventType, string note)
+        {
+            //scrivere allarme su db remoto
+        }
+    }
+}
