57e29f0a7b1c9ca990246aca9fe950b8cb2e76a5
Francesco Wofford <francescowofford@gmail.com>
2018-01-17 14:21:34 +0100

    added attached properties

---
+++ WpfAppLogin/ValueConverters/BooleanToVisibilityConverter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace WpfAppLogin
+{
+    public class BooleanToVisibilityConverter : BaseValueConverter<BooleanToVisibilityConverter>
+    {
+        public override object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return (bool)value ? Visibility.Hidden : Visibility.Visible;
+        }
+
+
+        public override object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
