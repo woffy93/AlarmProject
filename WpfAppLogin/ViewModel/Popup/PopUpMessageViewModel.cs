291ec0866a19402c3b95f274289216c14f5fd3f3
Francesco Wofford <francescowofford@gmail.com>
2018-01-24 15:07:05 +0100

    merged map UI code from side project

---
+++ WpfAppLogin/ViewModel/Popup/PopUpMessageViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfAppLogin
+{
+    public class PopUpMessageViewModel:BaseViewModel
+    {
+        public String Title { get; set; }
+        public String Message { get; set; }
+    }
+}
