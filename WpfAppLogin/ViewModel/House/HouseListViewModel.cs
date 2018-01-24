08a47153b6040e22aca974d355a1fa47f1b3cc29
Francesco Wofford <francescowofford@gmail.com>
2018-01-24 19:21:40 +0100

    filling SideMenu from db (partial code merge)

--- WpfAppLogin/ViewModel/House/HouseListViewModel.cs
+++ WpfAppLogin/ViewModel/House/HouseListViewModel.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,32 +11,58 @@ namespace WpfAppLogin
     public class HouseListViewModel
     {
         public static HouseListViewModel Instance => new HouseListViewModel();
-        public List<HouseItemViewModel> Houses { get; set; }
+        public List<HouseItemViewModel> Houses { get; set; } = new List<HouseItemViewModel>();
 
         public HouseListViewModel()
         {
-            //Houses.Add(new HouseItemViewModel{ Name = "aa" });
-            //Houses.Add(new HouseItemViewModel { Name = "bb" });
-            Houses = new List<HouseItemViewModel>
-            {
-                new HouseItemViewModel
-                {
-                    Name="aa"
-                },
-                new HouseItemViewModel
-                {
-                    Name="bb"
-                },
-                new HouseItemViewModel
-                {
-                    Name="bb"
-                },
-                new HouseItemViewModel
-                {
-                    Name="bb"
-                },
 
-            };
+            SqlConnection conn = new SqlConnection(@"Server = tcp:serverallarmi.database.windows.net,1433;
+                                                    Initial Catalog = dballarmi; 
+                                                    Persist Security Info = False;
+                                                    User ID =cisco; Password =VMware1!; 
+                                                    MultipleActiveResultSets = False;
+                                                    Encrypt = True; TrustServerCertificate = False; 
+                                                    Connection Timeout = 30;");
+            conn.Open();
+            SqlCommand sc = new SqlCommand("SELECT nome, id FROM Luoghi order by Id", conn);
+            SqlDataReader reader;
+
+            reader = sc.ExecuteReader();
+            DataTable dt = new DataTable();
+            dt.Columns.Add("nome", typeof(string));
+            dt.Columns.Add("id", typeof(string));
+            dt.Load(reader);
+            foreach (DataRow row in dt.Rows)
+            {
+                HouseItemViewModel a = new HouseItemViewModel { Name = row[0].ToString() };
+                if (a != null)
+                    Houses.Add(a);
+            }
+            conn.Close();
+
+
+            //Houses.Add(new HouseItemViewModel { Name = "aa" });
+            //Houses.Add(new HouseItemViewModel { Name = "bb" });
+            //    Houses = new List<HouseItemViewModel>
+            //    {
+            //        new HouseItemViewModel
+            //        {
+            //            Name="aa"
+            //        },
+            //        new HouseItemViewModel
+            //        {
+            //            Name="bb"
+            //        },
+            //        new HouseItemViewModel
+            //        {
+            //            Name="bb"
+            //        },
+            //        new HouseItemViewModel
+            //        {
+            //            Name="bb"
+            //        },
+
+            //    };
         }
 
     }
