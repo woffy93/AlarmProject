ee23aa915537a059a7185f6852f014b7e4ee368b
Francesco Wofford <francescowofford@gmail.com>
2018-01-25 13:29:12 +0100

    fixes

--- WpfAppLogin/Controls/SideMenuControl.xaml.cs
+++ WpfAppLogin/Controls/SideMenuControl.xaml.cs
@@ -24,5 +24,11 @@ namespace WpfAppLogin
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            WpfStorico panel = new WpfStorico();
+            panel.Show();
+        }
     }
 }
